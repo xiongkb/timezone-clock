7cfb53f5384324b2e45ec9f2a645fb337ebf10b9
xiongkb <mkan-bx@hotmail.com>
2020-11-12 19:10:52 -0800

    made a button so when clicked, time will update with useState hook

--- src/Components/Time/time.js
+++ src/Components/Time/time.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./time.css";
 
-const time = () => {
-    let currentTime = new Date();
-    console.log(currentTime);
+const Time = () => {
+    
+    let currentTime = new Date().toString();
+    // console.log(currentTime);
+    // console.log(typeof(currentTime))
+    const [ displayTime, setDisplayTime] = useState(currentTime);
+    
 
     return (
         <div>
-            <p>The current time right now is {currentTime.toString()}</p>
+            <p>Time: {displayTime}</p>
+            <button onClick={() => setDisplayTime(new Date().toString())}>
+                check time
+            </button>
         </div>
     )
 };
 
-export default time;
+export default Time;
