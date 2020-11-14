73f0cff374b35ed143f1e820a750414ae92eec67
xiongkb <mkan-bx@hotmail.com>
2020-11-14 13:13:54 -0800

    added useEffect to help display seconds dynamically

--- src/Components/Time/time.js
+++ src/Components/Time/time.js
@@ -1,20 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./time.css";
 
 const Time = () => {
+
+    // let seconds;
+    const [displayTime, setDisplayTime] = useState({ year: 0, seconds: 0 });
+    // let currentTime = new Date().toString();
+
+    function translateWeekday(weekday) {
+        switch (new Date().getDay()) {
+            case 0:
+                weekday = "Mon";
+                break;
+            case 1:
+                weekday = "Tue";
+                break;
+            case 2:
+                weekday = "Wed";
+                break;
+            case 3:
+                weekday = "Thu";
+                break; 
+            case 4:
+                weekday = "Fri";
+                break;
+            case 5:
+                weekday = "Sat";
+                break;
+            case 6:
+                weekday = "Sun";
+                break;           
+            default:
+                weekday = " ";
+                break;
+        }
+    }
+
+    function runTime() { 
+        let time = new Date();
+        
+        setDisplayTime({
+            year: time.getFullYear(),
+            month: time.getMonth(),
+            hour: time.getHours(),
+            minutes: time.getMinutes(),
+            weekday: translateWeekday(time.getDay()),
+            seconds: time.getSeconds()
+        });
+        // --------------------------------
+        
+        console.log("time: " + JSON.stringify({
+            year: time.getFullYear(),
+            seconds: time.getSeconds()
+        }))
+        // --------------------------------
+    };
+
+    // const [ displayTime, setDisplayTime] = useState(currentTime);
     
-    let currentTime = new Date().toString();
-    // console.log(currentTime);
-    // console.log(typeof(currentTime))
-    const [ displayTime, setDisplayTime] = useState(currentTime);
-    
+
+    useEffect(() => {
+        runTime();
+         setInterval(runTime, 1000);
+        
+    }, [])
 
     return (
         <div>
-            <p>Time: {displayTime}</p>
-            <button onClick={() => setDisplayTime(new Date().toString())}>
+            <p>Seconds in time: {displayTime.seconds}</p>
+
+            {/* <button onClick={() => setDisplaySeconds(new Date().toString())}>
                 check time
-            </button>
+            </button> */}
         </div>
     )
 };
