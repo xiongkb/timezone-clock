88d43203b006a20c689691d320ae651313abc86c
xiongkb <mkan-bx@hotmail.com>
2020-11-19 15:19:58 -0800

    enlarged search box

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -4,25 +4,32 @@ import "./navbar.css";
 const Navbar = () => {
     const [searchState, setSearchState] = useState('close');
 
-    function openSearch() {
-        console.log("clicked")
+    function btnRotation() {
         let btn = document.getElementsByClassName("add-tz-btn");
         console.log(btn);
         btn[0].style.transform = 'rotate(90deg)';
-        setTimeout(() => setSearchState("open"), 3000)
-        
-        
+        btn[0].style.transition = 'rotate(90deg)';
+        setTimeout(() => setSearchState("open"), 1000)
+
     }
 
+    function btnRotationClose() {
+        let btn = document.getElementsByClassName("close-tz-btn");
+        console.log(btn);
+        btn[0].style.transform = 'rotate(180deg)';
+        setTimeout(() => setSearchState("close"), 1000)
+    }
+
+    // rendering items
     return (
         <div className="navBar">
             <h1 className="app-title">Time Station</h1>
             {searchState === 'open' ?
                 <div>
-                    <input className="search-bar" placeholder="Enter a timezone"></input>
-                    <button className="close-tz-btn" onClick={() => setSearchState("close")}>-</button>
+                    <input className="search-bar" placeholder="ex: America/Los-Angeles"></input>
+                    <button className="close-tz-btn" onClick={() => btnRotationClose()}> - </button>
                 </div> :
-                <button className="add-tz-btn" onClick={() => {openSearch() }}>+</button>
+                <button className="add-tz-btn" onClick={() => btnRotation()}>+</button>
             }
         </div>
     )
