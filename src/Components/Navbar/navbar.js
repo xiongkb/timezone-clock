f362ed3f56a471040075e0fab934cb63b367f1f7
xiongkb <mkan-bx@hotmail.com>
2020-11-20 17:37:19 -0800

    added icon logo to navbar

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./navbar.css";
+import logo from "./logo.png"
 
 const Navbar = (props) => {
 
     // rendering items
     return (
         <div className="navBar">
-            <h1 className="app-title">Time Station</h1>
+            <div>
+                <img src={logo} alt="Red Clock" className="clock-logo"></img>
+                <h1 className="app-title">Time Station</h1>
+            </div>
             <input 
                 className="search-bar" 
                 placeholder="ex: America/Los-Angeles" 
